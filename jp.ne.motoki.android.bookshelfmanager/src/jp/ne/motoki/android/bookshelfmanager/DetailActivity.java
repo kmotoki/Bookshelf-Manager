dcdb5c3fd21ae6c6fe78040ef44b24a73cdb41a4
Kiyoshi Motoki <kiyoshi.motoki@gmail.com>
2012-02-01 01:37:26 +0900

    impl a few

--- jp.ne.motoki.android.bookshelfmanager/src/jp/ne/motoki/android/bookshelfmanager/DetailActivity.java
+++ jp.ne.motoki.android.bookshelfmanager/src/jp/ne/motoki/android/bookshelfmanager/DetailActivity.java
@@ -3,14 +3,11 @@ package jp.ne.motoki.android.bookshelfmanager;
 import static jp.ne.motoki.android.bookshelfmanager.Constants.ISBN;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
