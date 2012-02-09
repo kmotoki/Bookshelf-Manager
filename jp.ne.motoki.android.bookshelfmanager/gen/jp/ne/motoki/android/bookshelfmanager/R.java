96ca4592a97d5c2df816d7148130c76860f352ad
Kiyoshi Motoki <kiyoshi.motoki@gmail.com>
2012-02-10 02:48:17 +0900

    make layout of detail

--- jp.ne.motoki.android.bookshelfmanager/gen/jp/ne/motoki/android/bookshelfmanager/R.java
+++ jp.ne.motoki.android.bookshelfmanager/gen/jp/ne/motoki/android/bookshelfmanager/R.java
@@ -9,7 +9,8 @@ package jp.ne.motoki.android.bookshelfmanager;
 
 public final class R {
     public static final class array {
-        public static final int status_array=0x7f050000;
+        public static final int category_array=0x7f050000;
+        public static final int status_array=0x7f050001;
     }
     public static final class attr {
     }
@@ -17,15 +18,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int about=0x7f070008;
-        public static final int barcode=0x7f070005;
-        public static final int detail=0x7f070003;
-        public static final int detail_container=0x7f070000;
-        public static final int help=0x7f070007;
-        public static final int no_image=0x7f070002;
-        public static final int settings=0x7f070006;
-        public static final int status=0x7f070004;
-        public static final int title=0x7f070001;
+        public static final int ScrollView1=0x7f070000;
+        public static final int about=0x7f070011;
+        public static final int barcode=0x7f07000e;
+        public static final int button_cancel=0x7f07000d;
+        public static final int button_detail=0x7f07000a;
+        public static final int button_register=0x7f07000c;
+        public static final int category=0x7f070006;
+        public static final int container_category=0x7f070004;
+        public static final int container_detail=0x7f070009;
+        public static final int container_top_pane=0x7f070002;
+        public static final int help=0x7f070010;
+        public static final int label_category=0x7f070005;
+        public static final int label_detail=0x7f07000b;
+        public static final int label_title=0x7f070007;
+        public static final int relativeLayout1=0x7f070001;
+        public static final int settings=0x7f07000f;
+        public static final int thumbnail=0x7f070003;
+        public static final int title=0x7f070008;
     }
     public static final class layout {
         public static final int detail=0x7f030000;
@@ -38,10 +48,16 @@ public final class R {
         public static final int about=0x7f040003;
         public static final int app_name=0x7f040001;
         public static final int barcode=0x7f040002;
-        public static final int detail=0x7f040006;
+        public static final int button_cancel=0x7f040009;
+        public static final int button_register=0x7f040008;
+        public static final int default_title=0x7f04000a;
+        public static final int detail=0x7f04000c;
         public static final int hello=0x7f040000;
         public static final int help=0x7f040004;
+        public static final int label_category=0x7f04000b;
+        public static final int label_detail=0x7f040006;
+        public static final int label_title=0x7f040007;
         public static final int settings=0x7f040005;
-        public static final int status=0x7f040007;
+        public static final int status=0x7f04000d;
     }
 }
